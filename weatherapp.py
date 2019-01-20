01c0458dd96614f0fd4773e7ce427e61ba693efa
Roman Shkapiak <r.shkapiak@gmail.com>
2019-01-20 20:56:46 +0200

    Output current temperature in Dnipro from AccuWeather

--- weatherapp.py
+++ weatherapp.py
@@ -1,3 +1,21 @@
 """This is my first project"""
 
-print('Hello, I am alive!')
+import html
+from urllib.request import urlopen, Request
+
+accu_url = 'https://www.accuweather.com/ru/ua/dnipro/322722/daily-weather-forecast/322722?day=1'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+accu_request = Request(accu_url, headers=headers)
+response = urlopen(accu_request).read()
+response = str(response)
+tag = response.find('<span class="large-temp">')
+start = tag + len('<span class="large-temp">')
+temperature = ''
+for chair in response[start:]:
+    if chair != '<':
+        temperature += chair
+    else:
+        break
+
+print('From AccuWeather:')
+print('Temperature: {}'.format(html.unescape(temperature)))
