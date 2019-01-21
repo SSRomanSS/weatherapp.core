5aecabc41a0ae410a629f86e0b465c1511eadd72
Roman Shkapiak <r.shkapiak@gmail.com>
2019-01-21 23:28:30 +0200

    Add temperature and weather conditions from rp5.ua

--- weatherapp.py
+++ weatherapp.py
@@ -12,22 +12,52 @@ response = str(response)
 
 tag = response.find('<span class="large-temp">')
 start = tag + len('<span class="large-temp">')
-temperature = ''
+temperature_accu = ''
 for chair in response[start:]:
     if chair != '<':
-        temperature += chair
+        temperature_accu += chair
     else:
         break
 
 tag = response.find('<span class="cond">')
 start = tag + len('<span class="cond">')
-conditions = ''
+conditions_accu = ''
 for chair in response[start:]:
     if chair != '<':
-        conditions += chair
+        conditions_accu += chair
     else:
         break
 
-print('From AccuWeather:\n')
-print('Temperature: {}'.format(html.unescape(temperature)))
-print('Weather conditions: {}'.format(conditions))
+rp5_url = 'http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0' \
+          '_%D0%B2_%D0%94%D0%BD%D0%B5%D0%BF%D1%80%D0%B5_' \
+          '(%D0%94%D0%BD%D0%B5%D0%BF%D1%80%D0%BE%D0%BF%D0' \
+          '%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B5)'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+rp5_request = Request(rp5_url, headers=headers)
+response = urlopen(rp5_request).read().decode('utf-8')
+response = str(response)
+
+tag = response.find('style="display: block;">')
+start = tag + len('style="display: block;">')
+temperature_rp5 = ''
+for chair in response[start:]:
+    if chair != '<':
+        temperature_rp5 += chair
+    else:
+        break
+
+tag = response.find('°F</span>, ')
+start = tag + len('°F</span>, ')
+conditions_rp5 = ''
+for chair in response[start:]:
+    if chair != '<':
+        conditions_rp5 += chair
+    else:
+        break
+
+print('From AccuWeather:')
+print('Temperature: {}'.format(html.unescape(temperature_accu)))
+print('Weather conditions: {}\n'.format(conditions_accu))
+print('From rp5.ua:')
+print('Temperature: {}'.format(html.unescape(temperature_rp5)))
+print('Weather conditions: {}'.format(conditions_rp5.capitalize()))
