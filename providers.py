ad70c503670675df29aa752b9d6a4dd844e1422e
Roman Shkapiak <r.shkapiak@gmail.com>
2019-03-15 12:20:08 +0200

    Refactoring with WeatherProvider classes

---
+++ providers.py
@@ -0,0 +1,442 @@
+# -*- coding: utf-8 -*-
+"""
+Weather providers
+"""
+import sys
+import configparser
+import os
+from pathlib import Path
+import hashlib
+import time
+from urllib.parse import quote
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from htmldom import htmldom
+
+import config
+
+
+class AccuWeatherProvider:
+    """
+    Accuweather.com
+    """
+    def __init__(self):
+
+        name, url = self.create_settings(site_set=None, refresh=False)
+        self.name = name
+        self.url = url
+
+    def get_page_content(self, url, refresh):
+        """
+
+        :param url:
+        :param refresh:
+        :return:
+        """
+        if self.get_cache(url) and self.cache_lifetime(url) and refresh is False:
+            content = self.get_cache(url)
+        else:
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+            request = Request(url, headers=headers)
+            content = urlopen(request).read()
+            self.save_cache(url, content)
+        page_content = BeautifulSoup(content, features="lxml")
+        return page_content
+
+    def get_cache_dir(self):
+        """
+
+        :return:
+        """
+        return Path.home() / config.CACHE_DIR
+
+    def url_hash(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        return hashlib.md5(url.encode('utf8')).hexdigest()
+
+    def save_cache(self, url, page_source):
+        """
+
+        :param url:
+        :param page_source:
+        :return:
+        """
+        cache_dir = self.get_cache_dir()
+        cache_file = self.url_hash(url)
+        if not cache_dir.exists():
+            cache_dir.mkdir(parents=True)
+
+        with (cache_dir / cache_file).open('wb') as file:
+            file.write(page_source)
+
+    def get_cache(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        cache = b''
+        cache_dir = self.get_cache_dir()
+        cache_file = self.url_hash(url)
+        if (cache_dir / cache_file).exists():
+            with (cache_dir / cache_file).open('rb') as file:
+                cache = file.read()
+        return cache
+
+    def cache_lifetime(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        cash_file_path = self.get_cache_dir() / self.url_hash(url)
+        return time.time() - cash_file_path.stat().st_mtime < config.TIME_CACHE
+
+    def weather_source(self, input_name):
+        """
+
+        :param input_name:
+        :return:
+        """
+        weather_site = config.WEATHER_SITE
+
+        if os.path.exists(self.get_settings_file()):
+            weather_site = {self.get_settings()[2]: (self.get_settings()[1],
+                                                     # URL це значення 'link' із файла конфігурації
+                                                     *weather_site[self.get_settings()[2]][1:])
+                            }
+        if input_name == '':
+            weather_site = {'accu': weather_site['accu']}
+            return weather_site
+        if input_name in weather_site:
+            weather_site = {input_name: weather_site[input_name]}
+            return weather_site
+
+    def get_weather_info(self, page_content, tags):
+        """
+
+        :param page_content:
+        :param tags:
+        :return:
+        """
+        content = page_content
+        if len(tags) > 1:
+            for i in range(len(tags) - 1):
+                content = content.find(tags[i][0], tags[i][1])
+            result = content.find(tags[-1][0], tags[-1][1]).text
+
+            if result == '':
+                result = content.find(tags[-1][0], tags[-1][1])['alt']
+        else:
+            result = content.find(tags[-1][0], tags[-1][1]).text
+        return result
+
+    def create_settings(self, site_set, refresh):
+        """
+
+        :param site_set:
+        :param refresh:
+        :return:
+        """
+        name = config.CITY
+        link = config.ACCU_URL
+        if not site_set:
+            return name, link
+        link_set, locations_list, location_name, location_link = \
+            config.ACCU_SET, config.ACCU_LIST, config.ACCU_NAME, config.ACCU_LINK
+        content = self.get_page_content(link_set, refresh)
+
+        while content:
+            search_list = []
+            search_content = content.find_all(locations_list[0][0], locations_list[0][1])
+
+            ind = 0
+            for i in search_content:
+                print(i.find(location_name[0][0], location_name[0][1]).text, '-', ind)
+                search_list.append((i.find(location_name[0][0], location_name[0][1]).text,
+                                    i.find(location_link[0][0], location_link[0][1])['href']))
+                ind += 1
+            if search_list:
+                try:
+                    select = int(input('Make your choice, input the number:\n>'))
+                except ValueError:
+                    sys.exit('You input not number')
+                content = self.get_page_content(search_list[select][1], refresh)
+                link = search_list[select][1]
+                name = search_list[select][0]
+            else:
+                break
+
+        self.save_settings(name, link, site_set)
+
+    def get_settings_file(self):
+        """
+
+        :return:
+        """
+        return str(Path.home() / config.CONFIG_FILE)
+
+    def save_settings(self, name, url, site_set):
+        """
+
+        :param name:
+        :param url:
+        :param site_set:
+        :return:
+        """
+        settings = configparser.RawConfigParser()  # якшо значення конфігурації
+        # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
+        # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
+        settings.add_section('SELECTED_LOCATION')
+        settings.set('SELECTED_LOCATION', 'shortcut', site_set)
+        settings.set('SELECTED_LOCATION', 'name', name)
+        settings.set('SELECTED_LOCATION', 'link', url)
+        settings.write(open(self.get_settings_file(), 'w', encoding='utf8'))
+
+    def get_settings(self):
+        """
+
+        :return
+        """
+        settings = configparser.RawConfigParser()  # якшо значення конфігурації
+        # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
+        # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
+        settings.read(self.get_settings_file(), encoding='utf8')
+        name = settings.get('SELECTED_LOCATION', 'name')
+        url = settings.get('SELECTED_LOCATION', 'link')
+        site_set = settings.get('SELECTED_LOCATION', 'shortcut')
+        return name, url, site_set
+
+    def clear_cache(self):
+        """
+
+        :return:
+        """
+        cache_dir = self.get_cache_dir()
+        if cache_dir.exists():
+            for file in os.listdir(str(cache_dir)):
+                if time.time() - (cache_dir / file).stat().st_mtime > config.TIME_CACHE:
+                    os.remove(str(cache_dir / file))
+
+
+class Rp5WeatherProvider:
+    """
+    RP5.ua
+    """
+
+    def __init__(self):
+
+        name, url = self.create_settings(site_set=None, refresh=False)
+        self.name = name
+        self.url = url
+
+    def get_page_content(self, url, refresh):
+        """
+
+        :param url:
+        :param refresh:
+        :return:
+        """
+        if self.get_cache(url) and self.cache_lifetime(url) and refresh is False:
+            content = self.get_cache(url)
+        else:
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+            request = Request(url, headers=headers)
+            content = urlopen(request).read()
+            self.save_cache(url, content)
+        page_content = BeautifulSoup(content, features="lxml")
+        return page_content
+
+    def get_cache_dir(self):
+        """
+
+        :return:
+        """
+        return Path.home() / config.CACHE_DIR
+
+    def url_hash(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        return hashlib.md5(url.encode('utf8')).hexdigest()
+
+    def save_cache(self, url, page_source):
+        """
+
+        :param url:
+        :param page_source:
+        :return:
+        """
+        cache_dir = self.get_cache_dir()
+        cache_file = self.url_hash(url)
+        if not cache_dir.exists():
+            cache_dir.mkdir(parents=True)
+
+        with (cache_dir / cache_file).open('wb') as file:
+            file.write(page_source)
+
+    def get_cache(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        cache = b''
+        cache_dir = self.get_cache_dir()
+        cache_file = self.url_hash(url)
+        if (cache_dir / cache_file).exists():
+            with (cache_dir / cache_file).open('rb') as file:
+                cache = file.read()
+        return cache
+
+    def cache_lifetime(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        cash_file_path = self.get_cache_dir() / self.url_hash(url)
+        return time.time() - cash_file_path.stat().st_mtime < config.TIME_CACHE
+
+    def weather_source(self, input_name):
+        """
+
+        :param input_name:
+        :return:
+        """
+        weather_site = config.WEATHER_SITE
+
+        if os.path.exists(self.get_settings_file()):
+            weather_site = {self.get_settings()[2]: (self.get_settings()[1],
+                                                     # URL це значення 'link' із файла конфігурації
+                                                     *weather_site[self.get_settings()[2]][1:])
+                            }
+        if input_name == '':
+            weather_site = {'rp5': weather_site['rp5']}
+            return weather_site
+        if input_name in weather_site:
+            weather_site = {input_name: weather_site[input_name]}
+            return weather_site
+
+    def get_weather_info(self, page_content, tags):
+        """
+
+        :param page_content:
+        :param tags:
+        :return:
+        """
+        content = page_content
+        if len(tags) > 1:
+            for i in range(len(tags) - 1):
+                content = content.find(tags[i][0], tags[i][1])
+            result = content.find(tags[-1][0], tags[-1][1]).text
+
+            if result == '':
+                result = content.find(tags[-1][0], tags[-1][1])['alt']
+        else:
+            result = content.find(tags[-1][0], tags[-1][1]).text
+        return result
+
+    def create_settings(self, site_set, refresh):
+        """
+
+        :param site_set:
+        :param refresh:
+        :return:
+        """
+        name = config.CITY
+        link = config.RP5_URL
+        if not site_set:
+            return name, link
+        link_set, locations_list, location_name, location_link = \
+            config.RP5_SET, config.RP5_LIST, config.RP5_NAME, config.RP5_LINK
+        content = self.get_page_content(link_set, refresh)
+        dom = htmldom.HtmlDom(link_set)
+        dom = dom.createDom()
+
+        while content:
+            search_list = []
+            if len(locations_list) == 1:
+                search_content = content.find_all(locations_list[0][0], locations_list[0][1])
+                if not search_content:
+                    search_content = content.find_all(config.RP5_VAR[0], config.RP5_VAR[1])
+                    # для деяких кінцевих сторінок з RP5, які мають іншу структуру тегів
+            else:
+                search_content = content.find_all(locations_list[0][0], locations_list[0][1])
+                locations_list = locations_list[1:]
+
+            ind = 0
+            for i in search_content:
+                print(i.find(location_name[0][0], location_name[0][1]).text, '-', ind)
+                search_list.append((i.find(location_name[0][0], location_name[0][1]).text,
+                                    i.find(location_link[0][0], location_link[0][1])['href']))
+                ind += 1
+            if search_list:
+                try:
+                    select = int(input('Make your choice, input the number:\n>'))
+                except ValueError:
+                    sys.exit('You input not number')
+                content = self.get_page_content(
+                    dom.baseURL + '/' +
+                    quote(search_list[select][1], encoding='utf8'), refresh)
+                link = dom.baseURL + '/' + quote(search_list[select][1])
+                name = search_list[select][0]
+            else:
+                break
+
+        self.save_settings(name, link, site_set)
+
+    def get_settings_file(self):
+        """
+
+        :return:
+        """
+        return str(Path.home() / config.CONFIG_FILE)
+
+    def save_settings(self, name, url, site_set):
+        """
+
+        :param name:
+        :param url:
+        :param site_set:
+        :return:
+        """
+        settings = configparser.RawConfigParser()  # якшо значення конфігурації
+        # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
+        # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
+        settings.add_section('SELECTED_LOCATION')
+        settings.set('SELECTED_LOCATION', 'shortcut', site_set)
+        settings.set('SELECTED_LOCATION', 'name', name)
+        settings.set('SELECTED_LOCATION', 'link', url)
+        settings.write(open(self.get_settings_file(), 'w', encoding='utf8'))
+
+    def get_settings(self):
+        """
+
+        :return
+        """
+        settings = configparser.RawConfigParser()  # якшо значення конфігурації
+        # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
+        # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
+        settings.read(self.get_settings_file(), encoding='utf8')
+        name = settings.get('SELECTED_LOCATION', 'name')
+        url = settings.get('SELECTED_LOCATION', 'link')
+        site_set = settings.get('SELECTED_LOCATION', 'shortcut')
+        return name, url, site_set
+
+    def clear_cache(self):
+        """
+
+        :return:
+        """
+        cache_dir = self.get_cache_dir()
+        if cache_dir.exists():
+            for file in os.listdir(str(cache_dir)):
+                if time.time() - (cache_dir / file).stat().st_mtime > config.TIME_CACHE:
+                    os.remove(str(cache_dir / file))
--- weatherapp.py
+++ weatherapp.py
@@ -4,179 +4,10 @@ This is weather search script
 """
 import sys
 import argparse
-import configparser
 import html
 import os
-from pathlib import Path
-import hashlib
-import time
-from urllib.parse import quote
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
-from htmldom import htmldom
-
-
-ACCU_SET = 'https://www.accuweather.com/ru/browse-locations'
-ACCU_LIST = (['li', {'class': 'drilldown cl'}],)
-ACCU_NAME = (['em', {}],)
-ACUU_LINK = (['a', {}],)
-ACCU_URL = 'https://www.accuweather.com/ru/ua/dnipro/322722/weather-forecast/322722'
-ACCU_TAGS_TEMP = (['span', {'class': 'large-temp'}],)
-ACCU_TAGS_COND = (['span', {'class': 'cond'}],)
-
-RP5_SET = 'http://rp5.ua'
-RP5_LIST = (['div', {'class': 'country_map_links'}],
-            ['h3', {}],)
-RP5_NAME = (['a', {'href': True}],)
-RP5_LINK = (['a', {'href': True}],)
-RP5_VAR = ('div', {'class': 'city_link'})  # для додатокового пошуку на деяких кінцевих сторінквх
-RP5_URL = 'http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_' \
-          '%D0%94%D0%BD%D0%B5%D0%BF%D1%80%D0%B5_(%D0%94%D0%BD%D0%B5%D0%' \
-          'BF%D1%80%D0%BE%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B5)'
-RP5_TAGS_TEMP = (['div', {'class': 'ArchiveTemp'}],
-                 ['span', {'class': 't_0'}])
-RP5_TAGS_COND = (['div', {'id': 'archiveString'}],
-                 ['div', {'class': 'ArchiveInfo'}])
-
-SINOPTIK_URL = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0' \
-               '-%D0%B4%D0%BD%D0%B5%D0%BF%D1%80-303007131'
-SINOPTIK_TAGS_TEMP = (['p', {'class': 'today-temp'}],)
-SINOPTIK_TAGS_COND = (['div', {'class': 'lSide'}],
-                      ['div', {'class': 'img'}],
-                      ['img', {'height': '150'}])
-
-CITY = 'Dnipro'
-
-CONFIG_FILE = 'weatherapp.ini'
-
-CACHE_DIR = '.weather_cache'
-
-TIME_CACHE = 300
-
-
-def get_page_content(url, refresh):
-    """
-
-    :param url:
-    :param refresh:
-    :return:
-    """
-    if get_cache(url) and cache_lifetime(url) and refresh is False:
-        content = get_cache(url)
-    else:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-        request = Request(url, headers=headers)
-        content = urlopen(request).read()
-        save_cache(url, content)
-    page_content = BeautifulSoup(content, features="lxml")
-    return page_content
-
-
-def get_cache_dir():
-    """
-
-    :return:
-    """
-    return Path.home() / CACHE_DIR
-
-
-def url_hash(url):
-    """
-
-    :param url:
-    :return:
-    """
-    return hashlib.md5(url.encode('utf8')).hexdigest()
-
-
-def save_cache(url, page_source):
-    """
-
-    :param url:
-    :param page_source:
-    :return:
-    """
-    cache_dir = get_cache_dir()
-    cache_file = url_hash(url)
-    if not cache_dir.exists():
-        cache_dir.mkdir(parents=True)
-
-    with (cache_dir / cache_file).open('wb') as file:
-        file.write(page_source)
-
-
-def get_cache(url):
-    """
-
-    :param url:
-    :return:
-    """
-    cache = b''
-    cache_dir = get_cache_dir()
-    cache_file = url_hash(url)
-    if (cache_dir / cache_file).exists():
-        with (cache_dir / cache_file).open('rb') as file:
-            cache = file.read()
-    return cache
-
-
-def cache_lifetime(url):
-    """
-
-    :param url:
-    :return:
-    """
-    cash_file_path = get_cache_dir() / url_hash(url)
-    return time.time() - cash_file_path.stat().st_mtime < TIME_CACHE
-
-
-def weather_source(input_name):
-    """
-
-    :param input_name:
-    :return:
-    """
-    commands = {'accu': 'Accuweather',
-                'rp5': 'RP5',
-                'sin': 'sinoptik.ua'
-                }
-    weather_site = {'Accuweather': (ACCU_URL, ACCU_TAGS_TEMP, ACCU_TAGS_COND),
-                    'RP5': (RP5_URL, RP5_TAGS_TEMP, RP5_TAGS_COND),
-                    'sinoptik.ua': (SINOPTIK_URL, SINOPTIK_TAGS_TEMP, SINOPTIK_TAGS_COND)}
-
-    if os.path.exists(get_settings_file()):
-        weather_site_key = commands[get_settings()[2]]  # значення 'shortcut' із файла конфігурації
-    # є ключем в словнику 'command'
-        weather_site = {weather_site_key:
-                        (get_settings()[1],  # URL це значення 'link' із файла конфігурації
-                         *weather_site[weather_site_key][1:])}
-    if input_name == '':
-        return weather_site
-    if input_name in commands:
-        weather_site = {commands[input_name]:
-                        weather_site[commands[input_name]]}
-        return weather_site
-    return sys.exit('Unknown command, choice from: accu, rp5, sin')
-
-
-def get_weather_info(page_content, tags):
-    """
-
-    :param page_content:
-    :param tags:
-    :return:
-    """
-    content = page_content
-    if len(tags) > 1:
-        for i in range(len(tags) - 1):
-            content = content.find(tags[i][0], tags[i][1])
-        result = content.find(tags[-1][0], tags[-1][1]).text
-
-        if result == '':
-            result = content.find(tags[-1][0], tags[-1][1])['alt']
-    else:
-        result = content.find(tags[-1][0], tags[-1][1]).text
-    return result
+import providers
+import config
 
 
 def create_parser():
@@ -197,113 +28,19 @@ def create_parser():
     return parser
 
 
-def create_settings(site_set, refresh):
-    """
-
-    :param site_set:
-    :param refresh:
-    :return:
-    """
-    commands = {'accu': (ACCU_SET, ACCU_LIST, ACCU_NAME, ACUU_LINK),
-                'rp5': (RP5_SET, RP5_LIST, RP5_NAME, RP5_LINK),
-                }
-    if site_set not in commands:
-        sys.exit('Unknown command, choice from: accu, rp5, reset')
-    link_set, locations_list, location_name, location_link = commands[site_set]
-    content = get_page_content(link_set, refresh)
-    if site_set == 'rp5':  # на RP5 в структурі сторінки неповні лінки
-        dom = htmldom.HtmlDom(link_set)
-        dom = dom.createDom()
-
-    while content:
-        search_list = []
-        if len(locations_list) == 1:
-            search_content = content.find_all(locations_list[0][0], locations_list[0][1])
-            if not search_content:
-                search_content = content.find_all(RP5_VAR[0], RP5_VAR[1])  # для деяких кінцевих
-                # сторінок з RP5, які мають іншу структуру тегів
-        else:
-            search_content = content.find_all(locations_list[0][0], locations_list[0][1])
-            locations_list = locations_list[1:]
-
-        ind = 0
-        for i in search_content:
-            print(i.find(location_name[0][0], location_name[0][1]).text, '-', ind)
-            search_list.append((i.find(location_name[0][0], location_name[0][1]).text,
-                                i.find(location_link[0][0], location_link[0][1])['href']))
-            ind += 1
-        if search_list:
-            try:
-                select = int(input('Make your choice, input the number:\n>'))
-            except ValueError:
-                sys.exit('You input not number')
-            if 'http' not in search_list[select][1]:  # якщо лінк не повний
-                content = get_page_content(
-                    dom.baseURL + '/' +
-                    quote(search_list[select][1], encoding='utf8'), refresh)  # лінк кирилицею
-                link = dom.baseURL + '/' + quote(search_list[select][1])
-            if 'http' in search_list[select][1]:
-                content = get_page_content(search_list[select][1], refresh)
-                link = search_list[select][1]
-            name = search_list[select][0]
-        else:
-            break
-    save_settings(name, link, site_set)
-
-
-def get_settings_file():
-    """
-
-    :return:
-    """
-    return str(Path.home() / CONFIG_FILE)
-
-
-def save_settings(name, link, site_set):
-    """
-
-    :param name:
-    :param link:
-    :param site_set:
-    :return:
-    """
-    config = configparser.RawConfigParser()  # якшо значення конфігурації
-    # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
-    # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
-    config.add_section('SELECTED_LOCATION')
-    config.set('SELECTED_LOCATION', 'shortcut', site_set)
-    config.set('SELECTED_LOCATION', 'name', name)
-    config.set('SELECTED_LOCATION', 'link', link)
-    config.write(open(get_settings_file(), 'w', encoding='utf8'))
-
-
-def get_settings():
-    """
-
-    :return
-    """
-    config = configparser.RawConfigParser()  # якшо значення конфігурації
-    # зчитувати за допомогою ConfigParser,це  робить неможливим використання "%".
-    # Використано RawConfigParser, щоб уникнути спеціальної обробки значень конфігурації
-    config.read(get_settings_file(), encoding='utf8')
-    name = config.get('SELECTED_LOCATION', 'name')
-    link = config.get('SELECTED_LOCATION', 'link')
-    site_set = config.get('SELECTED_LOCATION', 'shortcut')
-    return name, link, site_set
-
-
-def result_output(website, temperature, conditions):
+def result_output(website, temperature, conditions, provider):
     """
 
     :param website:
     :param temperature:
     :param conditions:
+    :param provider
     :return:
     """
-    if os.path.exists(get_settings_file()):
-        print(get_settings()[0])
+    if os.path.exists(provider.get_settings_file()):
+        print(provider.get_settings()[0])
     else:
-        print(CITY)
+        print(config.CITY)
     print('From {}:'.format(website))
     print('Temperature: {}'
           .format(html.unescape(temperature.replace(' ', ''))))
@@ -311,19 +48,20 @@ def result_output(website, temperature, conditions):
           .format(conditions.strip()).replace(', ', '\n'))
 
 
-def file_output(result, out_file):
+def file_output(result, out_file, provider):
     """
 
     :param result:
     :param out_file:
+    :param provider:
     :return:
     """
 
     for name in result:
-        if os.path.exists(get_settings_file()):
-            print(get_settings()[0], file=out_file)
+        if os.path.exists(provider.get_settings_file()):
+            print(provider.get_settings()[0], file=out_file)
         else:
-            print(CITY, file=out_file)
+            print(config.CITY, file=out_file)
         print('From {}:\n'.format(name),
               'Temperature: {}\n'.format(html.unescape(result[name][0].replace(' ', ''))),
               'Weather conditions:\n {}\n'.format(result[name][1].strip().replace(', ', '\n')),
@@ -336,27 +74,44 @@ def main():
 
     :return:
     """
+    providers.AccuWeatherProvider().clear_cache()
+    known_command = {'accu': providers.AccuWeatherProvider(),
+                     'rp5': providers.Rp5WeatherProvider()
+                     }
+
     input_name = create_parser().parse_args(sys.argv[1:]).site
     out_file = create_parser().parse_args(sys.argv[1:]).file
     site_set = create_parser().parse_args(sys.argv[1:]).settings
     refresh = create_parser().parse_args(sys.argv[1:]).refresh
-    if input_name and os.path.exists(get_settings_file()):
+    if input_name not in known_command and input_name != '' and site_set not in known_command:
+        sys.exit('Unknown command')
+    if input_name and os.path.exists(known_command[input_name].get_settings_file()):
         sys.exit('Reset the settings: [-s] reset')
-    if site_set == 'reset' and os.path.exists(get_settings_file()):
-        os.remove(get_settings_file())
-    if site_set and site_set != 'reset':
-        create_settings(site_set, refresh)
+    if site_set == 'reset' and os.path.exists(providers.AccuWeatherProvider().get_settings_file()):
+        os.remove(providers.AccuWeatherProvider().get_settings_file())
+    if site_set in known_command and site_set != 'reset':
+        known_command[site_set].create_settings(site_set, refresh)
+
     out_dict = {}
-    for name in weather_source(input_name):
-        url, temp_tags, cond_tags = weather_source(input_name)[name]
-        page_content = get_page_content(url, refresh)
-        temperature = get_weather_info(page_content, temp_tags)
-        conditions = get_weather_info(page_content, cond_tags)
-        result_output(name, temperature, conditions)
+    if site_set in known_command:
+        known_command = {site_set: known_command[site_set]}
+    if input_name in known_command:
+        known_command = {input_name: known_command[input_name]}
+    if os.path.exists(providers.AccuWeatherProvider().get_settings_file()):
+        known_command = {providers.AccuWeatherProvider().get_settings()[2]:
+                         known_command[providers.AccuWeatherProvider().get_settings()[2]]
+                         }
+
+    for name in known_command:
+        url, temp_tags, cond_tags = known_command[name].weather_source(input_name)[name]
+        page_content = known_command[name].get_page_content(url, refresh)
+        temperature = known_command[name].get_weather_info(page_content, temp_tags)
+        conditions = known_command[name].get_weather_info(page_content, cond_tags)
+        result_output(name, temperature, conditions, known_command[name])
         if out_file:
             out_dict[name] = (temperature, conditions)
     if out_file:
-        file_output(out_dict, out_file)
+        file_output(out_dict, out_file, providers.AccuWeatherProvider())
 
 
 if __name__ == '__main__':
